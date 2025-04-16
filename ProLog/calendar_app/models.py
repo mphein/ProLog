e18281c82960303af9586965142ea5e39acbe69c
mphein <130398478+mphein@users.noreply.github.com>
2025-04-16 14:21:13 -0700

    Created django project, calendar app, and project_management app

---
+++ ProLog/calendar_app/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Event(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    reminder_sent = models.BooleanField(default=False)
