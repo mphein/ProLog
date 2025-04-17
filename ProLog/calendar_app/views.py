af3d90b6aa6818a039c1083b74f45e81175cdd45
Shloak Sinha <ssinha8@ucsc.edu>
2025-04-17 16:46:10 -0700

    Update views.py
    REACT and Django working together

--- ProLog/calendar_app/views.py
+++ ProLog/calendar_app/views.py
@@ -1,15 +1,17 @@
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny
 from .models import Event
 from .serializers import EventSerializer
 
 # Create your views here.
 class UserEventListAPIView(ListAPIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
     serializer_class = EventSerializer
 
     def get_queryset(self):
         """
         Return the list of events for the currently authenticated user.
         """
-        return Event.objects.filter(user=self.request.user)
+        return Event.objects.all()
+        # return Event.objects.filter(user=self.request.user)
