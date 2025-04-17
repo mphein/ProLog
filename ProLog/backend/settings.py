6eab2d2ffab721c12a726acc815a21dc7d33fb26
Shloak Sinha <ssinha8@ucsc.edu>
2025-04-17 15:05:10 -0700

    REACT interacting with Django
    BUGS: Axios is connecting with REST API, but the GET request is currently responded with as FORBIDDEN  (403)

--- ProLog/backend/settings.py
+++ ProLog/backend/settings.py
@@ -37,12 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'calendar_app',
     'project_management',
     'rest_framework',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -52,7 +54,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'ProLog.urls'
+ROOT_URLCONF = 'backend.urls'
 
 TEMPLATES = [
     {
@@ -70,7 +72,13 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'ProLog.wsgi.application'
+# CORS settings
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:5173",
+# ]
+CORS_ALLOW_ALL_ORIGINS = True
+
+WSGI_APPLICATION = 'backend.wsgi.application'
 
 
 # Database
