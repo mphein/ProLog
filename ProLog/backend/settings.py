03fc467debb9e51cb500b897895366805fe5a4f5
Shloak Sinha <ssinha8@ucsc.edu>
2025-04-18 13:44:14 -0700

    JWT Authorization working

--- ProLog/backend/settings.py
+++ ProLog/backend/settings.py
@@ -73,10 +73,16 @@ TEMPLATES = [
 ]
 
 # CORS settings
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:5173",
-# ]
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
